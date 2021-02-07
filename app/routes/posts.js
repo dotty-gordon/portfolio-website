cdc0634248cbce184a4d2aea6497d27067d58d9f
dagordon <david.gordon@zalando.ie>
2021-02-07 23:12:20 +0000

    PW-13 Live reload of filtered articles

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -4,15 +4,21 @@ import { inject as service } from '@ember/service';
 export default class PostsRoute extends Route {
     @service store;
 
+    queryParams = {
+        section: {
+          refreshModel: true
+        }
+      };
+
     model(params) {
     	var postCatalog = this.store.findAll('post')
     	if(params.section == null) {
     		return postCatalog
     	} else {
     		return new Ember.RSVP.Promise(resolve => {
-		   postCatalog.then(posts => {
-		      resolve(posts.filterBy('section', params.section));
-		    });
+    		   postCatalog.then(posts => {
+    		      resolve(posts.filterBy('section', params.section));
+    		    });
 		  });
     	}
 	}
